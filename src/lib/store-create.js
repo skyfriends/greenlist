5e5de085fa83b81fcf719800d97406128a6d0afe
Joshua Evans <skyfriends1@gmail.com>
2017-08-13 16:03:25 -0700

    deleeted eerryryryyryrthing

--- src/lib/store-create.js
+++
@@ -1,7 +0,0 @@
-import { createStore, applyMiddleware } from 'redux';
-import reducer from '../reducer';
-import thunk from './redux-thunk.js';
-import reporter from './redux-reporter.js';
-
-
-export default () => createStore(reducer, applyMiddleware(thunk, reporter));
