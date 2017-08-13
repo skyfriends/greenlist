a814d1fe389e8a014693e866d4adb4393b439e69
Michael Stuart <mike.w.stuart@gmail.com>
2017-08-13 16:32:28 -0700

    mongodb

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import express from 'express';
 import renderApp from './render-app';
+import mongoose from 'mongoose';
+import {} from 'dotenv/config';
 
 const app = express();
 
 app.use('/static', express.static('dist'));
 app.use('/static', express.static('public'));
+
+const mongodb = `mongodb://${process.env.UNAME}:${process.env.PASS}@${process.env.LOC}:${process.env.MDBPORT}/greenlist`;
+mongoose.connect(mongodb);
 
 app.get('*', (req, res) => res.send(renderApp()));
 
