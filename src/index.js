fe9d5879c70c9c3fe8bae705d69ddf5c2343d304
Michael Stuart <mike.w.stuart@gmail.com>
2017-08-13 18:35:55 -0700

    comment out favicons for now

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import {} from 'dotenv/config';
 import express from 'express';
 import renderApp from './render-app';
 import mongoose from 'mongoose';
-import favicon from 'serve-favicon';
+// import favicon from 'serve-favicon';
 
 const app = express();
 
 app.use('/static', express.static('dist'));
 app.use('/static', express.static('public'));
-app.use(favicon(path.join(__dirname, '..', '..', 'public', 'favicon.ico')));
+// app.use(favicon(path.join(__dirname, '..', '..', 'public', 'favicon.ico')));
 
 const mongodb = `mongodb://${process.env.UNAME}:${process.env.PASS}@${process.env.LOC}:${process.env.MDBPORT}/greenlist`;
 mongoose.connect(mongodb);
