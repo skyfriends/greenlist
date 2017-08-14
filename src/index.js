d36fafd22a90a457da4048a580757ad1d489fa27
Michael Stuart <mike.w.stuart@gmail.com>
2017-08-13 18:32:13 -0700

    fix port finally

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
+import {} from 'dotenv/config';
 import express from 'express';
 import renderApp from './render-app';
 import mongoose from 'mongoose';
-import {} from 'dotenv/config';
+import favicon from 'serve-favicon';
 
 const app = express();
 
 app.use('/static', express.static('dist'));
 app.use('/static', express.static('public'));
+app.use(favicon(path.join(__dirname, '..', '..', 'public', 'favicon.ico')));
 
 const mongodb = `mongodb://${process.env.UNAME}:${process.env.PASS}@${process.env.LOC}:${process.env.MDBPORT}/greenlist`;
 mongoose.connect(mongodb);
@@ -25,4 +27,4 @@ app.get('*', (req, res) => {
   // })
 })
 
-app.listen(WEB_PORT || 8080);
+app.listen(process.env.PORT || 8080);
