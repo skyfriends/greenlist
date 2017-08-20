6ff83cefaafe696857aaa1d9d309544f8e21a056
Joshua Evans <skyfriends1@gmail.com>
2017-08-19 21:12:13 -0700

    styled up list a little bit with some material

--- src/component/app/index.js
+++ src/component/app/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import superagent from 'superagent';
 import DataTable from '../data-table';
 import ShittyList from '../shitty-list';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+import AppBar from 'material-ui/AppBar';
 
 class App extends React.Component {
   constructor(props) {
@@ -10,19 +13,21 @@ class App extends React.Component {
   }
 
   componentDidMount() {
-    superagent.get('http://localhost:8080/data')
+    superagent
+      .get('http://localhost:8080/data')
       .then(res => this.setState({ data: JSON.parse(res.text) }));
   }
 
   render() {
     return (
-      <div>
-        {this.state.data ? (
-          <ShittyList data={this.state.data} />
-        ) : (
-          <h1>Loading...</h1>
-        )}
-      </div>
+      <MuiThemeProvider>
+        <div>
+          <AppBar title="Green List" style={{ backgroundColor: 'Green' }} />
+          {this.state.data
+            ? <ShittyList data={this.state.data} />
+            : <h1>Loading...</h1>}
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
--- src/component/shitty-list/index.js
+++ src/component/shitty-list/index.js
@@ -1,18 +1,25 @@
 import React from 'react';
+import { List, ListItem } from 'material-ui/List';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
-const ShittyList = props => (
-  <ul>
-    {props.data.map((item, index) =>
-      <li key={item._id}>
-        <h3>{`#${index + 1}`}</h3>
-        <p>{item.a}</p>
-        <p>{`${item.c}, ${item.s}`}</p>
-        <p>{item.z}</p>
-        <p>{`Builder: ${item.builder}`}</p>
-        <p>{`Certification Type: ${item.certType}`}</p>
-      </li>
-    )}
-  </ul>
-);
+const ShittyList = props =>
+  <MuiThemeProvider>
+    <List>
+      {props.data.map((item, index) =>
+        <ListItem key={item._id}>
+          <h3>{`#${index + 1}`}</h3>
+          <p>
+            {item.a}
+          </p>
+          <p>{`${item.c}, ${item.s}`}</p>
+          <p>
+            {item.z}
+          </p>
+          <p>{`Builder: ${item.builder}`}</p>
+          <p>{`Certification Type: ${item.certType}`}</p>
+        </ListItem>
+      )}
+    </List>
+  </MuiThemeProvider>;
 
 export default ShittyList;
